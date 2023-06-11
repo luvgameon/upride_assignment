6174c05fc82a9fad7c75701d50bb8446468883a6
luvgameon <luvgameon@gmail.com>
2023-06-11 16:25:25 +0530

    topbar

---
+++ src/Components/Navbar/TopBar.js
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import "./TopBar.css";
+import plusicon from '../../assets/plus.svg';
+import searchicon from '../../assets/searchicon.svg';
+
+
+export default function TopBar() {
+    const [searchText, setSearchText] = useState("");
+    return (
+        <div className="topbar">
+            <div className="searchbar">
+                <input type="text" placeholder="Search Bookings" className="searchbarinput" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
+                <img src={searchicon} alt="search" title="search" className="searchicon" />
+            </div>
+            <div className="newbookingbutton">
+                <img src={plusicon} alt="plus" className="" />
+                New Booking
+            </div>
+         
+        </div>
+    )
+}
